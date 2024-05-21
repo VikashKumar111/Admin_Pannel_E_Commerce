27874512641a74b006de69a9c25e4db7aa7e3c62
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-21 17:56:43 +0530

    add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div className="py=5" style={{ background: "#ffd333" }}>
+      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-3" ></div>
+  </div>;
 };
 
 export default Login;
