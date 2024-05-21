5bb8c013be88fc1d32c00689eda2a33fe4e58642
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-21 17:02:46 +0530

    add

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
-const { Sider, Content } = Layout;
-
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+const { Header, Sider, Content } = Layout;
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -18,45 +20,51 @@ const MainLayout = () => {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          defaultSelectedKeys={['1']}
           items={[
             {
-              key: "1",
+              key: '1',
               icon: <UserOutlined />,
-              label: "nav 1",
+              label: 'nav 1',
             },
             {
-              key: "2",
+              key: '2',
               icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              label: 'nav 2',
             },
             {
-              key: "3",
+              key: '3',
               icon: <UploadOutlined />,
-              label: "nav 3",
+              label: 'nav 3',
             },
           ]}
         />
       </Sider>
       <Layout>
-        
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
           <Button
             type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
-              fontSize: "16px",
+              fontSize: '16px',
               width: 64,
               height: 64,
             }}
           />
-        
+        </Header>
         <Content
           style={{
-            margin: "24px 16px",
+            margin: '24px 16px',
             padding: 24,
             minHeight: 280,
-            
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
           }}
         >
           Content
@@ -65,5 +73,4 @@ const MainLayout = () => {
     </Layout>
   );
 };
-
 export default MainLayout;
