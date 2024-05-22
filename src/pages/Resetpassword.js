83ad7d7e27243365ce068df750e08ac415079987
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-22 14:46:56 +0530

    add

--- src/pages/Resetpassword.js
+++ src/pages/Resetpassword.js
@@ -7,14 +7,15 @@ const Resetpassword = () => {
       <br />
       <div className="my-5  bg-white rounded-3 mx-auto p-4" style={{ width: "32%" }}>
         <h3 className="text-center">Forgot Password</h3>
-        <p className="text-center">Please Enter your registered Email to get reset password mail</p>
+        <p className="text-center">Please Enter your new password</p>
         <form action="">
-          <CustomInput type="text" label="Email" id="email" />
+          <CustomInput type="text" label="New Password" id="email" />
+          <CustomInput type="text" label="Confirm Password" id="email" />
           <button
             className="border-0 px-3 py-2 text-white fw-bold w-100"
             style={{ background: "#ffd333" }}
             type="submit"
-          >Send Link</button>
+          >Reset Password</button>
         </form>
       </div>
     </div>
