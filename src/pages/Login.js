72829ffc1da8cb62d1055c5d6d5feafe6bad6316
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-22 14:00:35 +0530

    add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,7 @@ const Login = () => {
     <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
       <br />
       <br />
-      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+      <div className="my-5 bg-white rounded-3 mx-auto p-4" style={{ width: "28%" }}>
         <h3 className="text-center">Login</h3>
         <p className="text-center">Login to your account to continue</p>
         <form action="">
--- src/pages/Resetpassword.js
+++ src/pages/Resetpassword.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import CustomInput from "../components/Custominput";
 const Resetpassword = () => {
-  return <div>Resetpassword</div>;
+  return (
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <div className="my-5  bg-white rounded-3 mx-auto p-4" style={{ width: "32%" }}>
+        <h3 className="text-center">Forgot Password</h3>
+        <p className="text-center">Please Enter your registered Email to get reset password mail</p>
+        <form action="">
+          <CustomInput type="text" label="Email" id="email" />
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >Send Link</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Resetpassword;
