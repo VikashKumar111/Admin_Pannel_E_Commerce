13d70bebb7564a0c8935233c45daba66be90e8a1
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-25 11:53:52 +0530

    add

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,31 @@
 import React from "react";
-import { BsArrowDownRight} from "react-icons/bs";
+import { BsArrowDownRight } from "react-icons/bs";
 import { Column } from "@ant-design/plots";
+import { Table } from "antd";
 
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+];
+const data1 = [];
+for (let i = 0; i < 46; i++) {
+  data1.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
 const Dashboard = () => {
   const data = [
     {
@@ -58,8 +82,8 @@ const Dashboard = () => {
     xField: "type",
     yField: "sales",
     color: ({ type }) => {
-    return '#ffd333'; 
-  },
+      return "#ffd333";
+    },
     label: {
       position: "top",
       style: {
@@ -93,11 +117,10 @@ const Dashboard = () => {
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6>
-              {" "}
               <BsArrowDownRight />
               32%
             </h6>
-            <p>Compared to April 2022</p>
+            <p className="mb-0">Compared to April 2022</p>
           </div>
         </div>
         <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
@@ -107,11 +130,10 @@ const Dashboard = () => {
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6 className="red">
-              {" "}
               <BsArrowDownRight />
               32%
             </h6>
-            <p>Compared to April 2022</p>
+            <p className="mb-0">Compared to April 2022</p>
           </div>
         </div>
         <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
@@ -121,18 +143,28 @@ const Dashboard = () => {
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6 className="green">
-              {" "}
               <BsArrowDownRight />
               32%
             </h6>
-            <p>Compared to April 2022</p>
+            <p className="mb-0">Compared to April 2022</p>
           </div>
         </div>
       </div>
-      <div className="mt-4">
-          <h3>Income Statistics</h3>
-          <div>  <Column {...config} /></div>    
-      </div>    
+      <div className="mt-3">
+        <h3>Income Statistics</h3>
+        <div>
+          <Column {...config} />
+        </div>
+      </div>
+      <div className="mt-5">
+        <h3 className="mb-4">Recent Orders</h3>
+        <div>
+          <Table
+            columns={columns}
+            dataSource={data1}
+          />
+        </div>
+      </div>
     </div>
   );
 };
