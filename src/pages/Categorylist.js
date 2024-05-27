00341c9d6a04ad0ae720ac0f7a739656f3a2cdf0
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-27 11:01:16 +0530

    add

--- src/pages/Categorylist.js
+++ src/pages/Categorylist.js
@@ -1,6 +1,36 @@
 import React from "react";
 import { Table } from "antd";
 
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Phone Number",
+    dataIndex: "number",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+];
+const data1 = [];
+for (let i = 0; i < 88; i++) {
+  data1.push({
+    key: i,
+    name: `Anuj Kumar ${i}`,
+    age: 28,
+    number: 9318667788,
+    address: `Saharanpur, Park Lane no. ${i}`,
+  });
+}
+
+
 const Categorylist = () => {
   return (
     <div>
