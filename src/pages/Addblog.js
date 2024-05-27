f5ce2fb598ee19b664362fd9dfc7f6ab2f2a840e
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-27 13:47:53 +0530

    add

--- src/pages/Addblog.js
+++ src/pages/Addblog.js
@@ -1,8 +1,17 @@
 import { React } from "react";
+import CustomInput from "../components/Custominput";
 
 const Addblog = () => {
     return <div>
         <h3 className="mb-4">Add Blog</h3>
+        <div className="">
+            <form action="">
+                <CustomInput type="text" label="Enter Blog Title" /> 
+                <select name="" id="">
+                      <option>Select Blog Category</option>
+                </select>
+            </form>
+        </div>
     </div>
 };
 
