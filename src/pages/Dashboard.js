a9f35c1015badb82cc4584ad5e04327d59ea030f
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-28 15:14:32 +0530

    add

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -117,41 +117,41 @@ const Dashboard = () => {
       <div className="d-flex justify-content-between align-items-center gap-3">
         <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
           <div>
-            <p className="">Total</p>
-            <h4 className="mb-0">$1100</h4>
+            <p className="desc">Total</p>
+            <h4 className="mb-0 sub-title">$1100</h4>
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6>
               <BsArrowDownRight />
               32%
             </h6>
-            <p className="mb-0">Compared to April 2022</p>
+            <p className="mb-0 desc">Compared to April 2022</p>
           </div>
         </div>
         <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
           <div>
-            <p className="">Total</p>
-            <h4 className="mb-0">$1100</h4>
+            <p className="desc">Total</p>
+            <h4 className="mb-0 sub-title">$1100</h4>
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6 className="red">
               <BsArrowDownRight />
               32%
             </h6>
-            <p className="mb-0">Compared to April 2022</p>
+            <p className="mb-0 desc">Compared to April 2022</p>
           </div>
         </div>
         <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
           <div>
-            <p className="">Total</p>
-            <h4 className="mb-0">$1100</h4>
+            <p className="desc">Total</p>
+            <h4 className="mb-0 sub-title">$1100</h4>
           </div>
           <div className="d-flex flex-column align-items-end">
             <h6 className="green">
               <BsArrowDownRight />
               32%
             </h6>
-            <p className="mb-0">Compared to April 2022</p>
+            <p className="mb-0 desc">Compared to April 2022</p>
           </div>
         </div>
       </div>
