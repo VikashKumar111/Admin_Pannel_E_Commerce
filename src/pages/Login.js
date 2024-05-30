13874c0dba68502883a52550587494ca6e08ffbf
Vikash Kumar <vikasthakur9310@gmail.com>
2024-05-30 15:31:19 +0530

    add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,17 @@ import React from "react";
 import CustomInput from "../components/Custominput";
 import { Link } from "react-router-dom";
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+let schema = Yup.object().shape({
+  email: Yup
+    .string()
+    .email("Email should be valid")
+    .required("Email is Required"),
+  password: Yup.string().required("Password is Required"),
+});
+
+
 
 const Login = () => {
 
@@ -10,6 +21,7 @@ const Login = () => {
        email: '',
        password:'',
      },
+     validationSchema:schema,
      onSubmit: values => {
        alert(JSON.stringify(values, null, 2));
      },
@@ -34,6 +46,9 @@ const Login = () => {
             onCh={formik.handleChange("email")}
             val={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+         <div>{formik.errors.email}</div> 
+       ) : null}
           <CustomInput
             type="text"
             name="password"
@@ -42,19 +57,21 @@ const Login = () => {
             onCh={formik.handleChange("password")}
             val={formik.values.password}
           />
+          {formik.touched.password && formik.errors.password ? (
+         <div>{formik.errors.password}</div>
+       ) : null}
           <div className="mb-3 text-end">
             <Link to="forgot-password" className="">
               Forgot Password?
             </Link>
           </div>
-          <Link
-            to="/admin"
+          <button
             className="border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5"
             style={{ background: "#ffd333" }}
             type="submit"
           >
             Login
-          </Link>
+          </button>
         </form>
       </div>
     </div>
