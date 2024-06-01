67bcdbba97e63806f817fb17f413db1e3ea94bd2
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-01 12:55:30 +0530

    add

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
+import productReducer from "../features/product/productSlice";
 
 
 export const store = configureStore({
-  reducer: { auth: authReducer, customer: customerReducer },
+  reducer: { auth: authReducer, customer: customerReducer , product: productReducer },
   devTools: process.env.NODE_ENV !== "production",
 });
