e99365ae3edb790deaca927b1b07eca01f57f45d
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-01 16:19:24 +0530

    add

--- src/features/brands/brandService.js
+++ src/features/brands/brandService.js
@@ -3,7 +3,7 @@ import { base_url } from "../../utils/base_url";
 
 
 
-export const getBrands = async () => {
+const getBrands = async () => {
     const response = await axios.get(`${base_url}brands/`);
     return response.data;
 };
