5a101fc56a5eecfdfbf3fa1afd024266171cb8c5
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-01 14:54:05 +0530

    add

--- src/pages/Productlist.js
+++ src/pages/Productlist.js
@@ -14,14 +14,17 @@ const columns = [
   {
     title: "Title",
     dataIndex: "title",
+    sorter: (a,b)=> a.title.length-b.title.length,
   },
   {
     title: "Brand",
     dataIndex: "brand",
+    sorter: (a,b)=> a.brand.length-b.brand.length,
   },
   {
     title: "Category",
     dataIndex: "category",
+    sorter: (a,b)=> a.category.length - b.category.length,
   },
   {
     title: "Color",
@@ -30,6 +33,7 @@ const columns = [
   {
     title: "Price",
     dataIndex: "price",
+    sorter: (a,b)=> a.price -b.price,
   },
   {
     title: "Action",
