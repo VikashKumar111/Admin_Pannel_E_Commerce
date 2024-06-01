6d6d0bac9d5d0c2b485b39e657d73c0562976476
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-01 12:53:13 +0530

    add

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,7 +1,6 @@
 import {
   createSlice,
   createAsyncThunk,
-  isRejectedWithValue,
 } from "@reduxjs/toolkit";
 import productService from "./productService";
 
@@ -16,11 +15,37 @@ const getProducts = createAsyncThunk(
   }
 );
 
-
 const initialState = {
-    products: [],
-    isError: false,
-    isLoading: false,
-    isSuccess: false,
-    message: "",
+  products: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
 };
+
+export const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.message = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      });
+  },
+});
+
+
+export default productSlice.reducer;
