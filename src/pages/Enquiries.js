ed6c04c47abcc7e2bba665c73d21b0b2eea9389c
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-04 16:18:47 +0530

    add

--- src/pages/Enquiries.js
+++ src/pages/Enquiries.js
@@ -1,48 +1,67 @@
-import React, { useEffect } from 'react';
-import { Table } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
-import { getEnquiry } from '../features/enquiry/enquirySlice';
+import React, { useEffect } from "react";
+import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getEnquiry } from "../features/enquiry/enquirySlice";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const columns = [
+  {
+    title: "SNo",
+    dataIndex: "key",
+  },
   {
     title: "Name",
     dataIndex: "name",
   },
   {
-    title: "Age",
-    dataIndex: "age",
+    title: "Email",
+    dataIndex: "email",
   },
   {
-    title: "Phone Number",
-    dataIndex: "number",
+    title: "Mobile",
+    dataIndex: "mobile",
   },
   {
-    title: "Address",
-    dataIndex: "address",
+    title: "Action",
+    dataIndex: "action",
   },
 ];
-const data1 = [];
-for (let i = 0; i < 88; i++) {
-  data1.push({
-    key: i,
-    name: `Anuj Kumar ${i}`,
-    age: 28,
-    number: 9318667788,
-    address: `Saharanpur, Park Lane no. ${i}`,
-  });
-}
 
 const Enquiries = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getEnquiry())
+    dispatch(getEnquiry());
   }, []);
-  const enqState = useSelector((state))
-    return <div>
-        <h3 className='mb-4 title'>Enquiries</h3>
-        <div>
-             <Table columns={columns} dataSource={data1} />
-        </div>
+  const enqState = useSelector((state) => state.enquiry.enquiry);
+  const data1 = [];
+  for (let i = 0; i < enqState.length; i++) {
+    data1.push({
+      key: i+1,
+      name: enqState[i].name,
+      email: enqState[i].email,
+      mobile: enqState[i].mobile,
+      action: (
+        <>
+          <Link className="fs-3 text-danger" to="/">
+            <BiEdit />
+          </Link>
+          <Link className="ms-3 fs-3 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
+    });
+  }
+
+  return (
+    <div>
+      <h3 className="mb-4 title">Enquiries</h3>
+      <div>
+        <Table columns={columns} dataSource={data1} />
+      </div>
     </div>
+  );
 };
 export default Enquiries;
