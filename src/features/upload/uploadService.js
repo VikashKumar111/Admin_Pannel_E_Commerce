8de96c425152a04ed841d7a3a01200fec6b62698
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-07 17:22:43 +0530

    add

---
+++ src/features/upload/uploadService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { base_url } from "../../utils/base_url";
+import { config } from "../../utils/axiosconfig";
+
+
+
+const uploadImg = async (data) => {
+    const response = await axios.post(`${base_url}upload`, data, config);
+    return response.data;
+};
+
+const uploadService = {
+    uploadImg,
+};
+
+
+export default uploadService;
