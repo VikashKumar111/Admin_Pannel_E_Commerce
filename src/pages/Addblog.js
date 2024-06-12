cd715641288d93ed907bd30e3c45a2a6aebf21d1
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-12 12:23:13 +0530

    add

--- src/pages/Addblog.js
+++ src/pages/Addblog.js
@@ -4,27 +4,7 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 import { InboxOutlined } from "@ant-design/icons";
-import { message, Upload } from "antd";
-const { Dragger } = Upload;
-const props = {
-  name: "file",
-  multiple: true,
-  action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
-};
+
 
 const Addblog = () => {
   const [desc, setDesc] = useState();
@@ -37,18 +17,7 @@ const Addblog = () => {
       <h3 className="mb-4 title">Add Blog</h3>
       <div className="">
         <form action="">
-          <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibited from
-              uploading company data or other banned files.
-            </p>
-                  </Dragger>
+         
          <div className="mt-4"> <CustomInput type="text" label="Enter Blog Title" /></div>
          
           <select name="" className="form-control py-3 mb-3" id="">
