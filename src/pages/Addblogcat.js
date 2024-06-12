0b1ffb4c916c6588d4db4842cf720a63061607b5
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-12 18:31:27 +0530

    add

--- src/pages/Addblogcat.js
+++ src/pages/Addblogcat.js
@@ -1,4 +1,88 @@
-import React from "react";
+// import React, { useEffect } from "react";
+// import CustomInput from "../components/Custominput";
+// import * as Yup from "yup";
+// import { useFormik } from "formik";
+// import { newBlogCategory } from "../features/bcategory/bcategorySlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { toast } from "react-toastify";
+
+// let schema = Yup.object().shape({
+//   title: Yup.string().required("Title is Required"),
+// });
+
+// const Addblogcat = () => {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const newBlogCat = useSelector((state) => state.bCategory);
+//   const { isSuccess, isError, isLoading, createdBlogCategory } = newBlogCat;
+    
+
+   
+    
+    
+//   const formik = useFormik({
+//     initialValues: {
+//       title: "",
+//     },
+//     validationSchema: schema,
+//     onSubmit: (values) => {
+//       // console.log("Submitting form with values:", values);
+//       dispatch(newBlogCategory(values));
+//       formik.resetForm();
+//       notification();
+//     },
+//   });
+
+//     const notification = () => {
+//       if (isSuccess && createdBlogCategory) {
+//         toast.success("Blog Added Successfully!");
+//         setTimeout(() => {
+//           navigate("/admin/blog-category-list");
+//         }, 3000);
+//       }
+//       if (isError) {
+//         toast.error("Something Went Wrong!");
+//       }
+//     };
+
+//   return (
+//     <div>
+//       <h3 className="mb-4 title">Add Blog Category</h3>
+//       <div>
+//         <form action="" onSubmit={formik.handleSubmit}>
+//           <CustomInput
+//             type="text"
+//             name="title"
+//             onChng={formik.handleChange("title")}
+//             onBlr={formik.handleBlur("title")}
+//             val={formik.values.title}
+//             label="Enter Blog Category"
+//             id="blogcat"
+//           />
+//           <div className="error mt-3">
+//             {formik.touched.title && formik.errors.title}
+//           </div>
+//           <button
+//             className="btn btn-success border-0 rounded-3 my-5"
+//             type="submit"
+//           >
+//             Add Blog Category
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Addblogcat;
+
+
+
+
+
+
+import React, { useEffect } from "react";
 import CustomInput from "../components/Custominput";
 import * as Yup from "yup";
 import { useFormik } from "formik";
@@ -17,30 +101,29 @@ const Addblogcat = () => {
 
   const newBlogCat = useSelector((state) => state.bCategory);
   const { isSuccess, isError, isLoading, createdBlogCategory } = newBlogCat;
+
   const formik = useFormik({
     initialValues: {
       title: "",
     },
     validationSchema: schema,
     onSubmit: (values) => {
-      // console.log("Submitting form with values:", values);
       dispatch(newBlogCategory(values));
       formik.resetForm();
-      notification();
     },
   });
 
-    const notification = () => {
-      if (isSuccess && createdBlogCategory) {
-        toast.success("Blog Added Successfully!");
-        setTimeout(() => {
-          navigate("/admin/blog-list");
-        }, 3000);
-      }
-      if (isError) {
-        toast.error("Something Went Wrong!");
-      }
-    };
+  useEffect(() => {
+    if (isSuccess && createdBlogCategory) {
+      toast.success("Blog Added Successfully!");
+      setTimeout(() => {
+        navigate("/admin/blog-category-list");
+      }, 3000);
+    }
+    if (isError) {
+      toast.error("Something Went Wrong!");
+    }
+  }, [isSuccess, isError, createdBlogCategory, navigate]);
 
   return (
     <div>
@@ -70,4 +153,5 @@ const Addblogcat = () => {
     </div>
   );
 };
+
 export default Addblogcat;
