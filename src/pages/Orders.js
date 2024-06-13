b0f87062a9785c2b08d487e8da01223bd74c3a4a
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-13 15:48:58 +0530

    add

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -39,18 +39,19 @@ const Orders = () => {
     dispatch(getOrders());
   }, []);
   const orderState = useSelector((state) => state.auth.orders);
+  console.log(orderState);
   const data1 = [];
   for (let i = 0; i < orderState.length; i++) {
     data1.push({
       key: i + 1,
       name: orderState[i].orderby.firstname,
-      product: orderState[i].products.map((i, j) => {
-        return (
-          <ul key={j}>
-            <li>{i.product.title}</li>
-          </ul>
-        );
-      }),
+      // product: orderState[i].products.map((i, j) => {
+      //   return (
+      //     <ul key={j}>
+      //       <li>{i.product.title}</li>
+      //     </ul>
+      //   );
+      // }),
       amount: orderState[i].paymentIntent.amount,
       date: new Date(orderState[i].createdAt).toDateString(),
       action: (
