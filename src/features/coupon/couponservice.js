a599fd666f69aec33b5f7365b936e084296b8bcf
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-14 14:55:13 +0530

    add

--- src/features/coupon/couponservice.js
+++ src/features/coupon/couponservice.js
@@ -3,4 +3,20 @@ import { base_url } from "../../utils/base_url";
 
 
 
-const 
+const getCoupons = async () => {
+    const response = await axios.get(`${base_url}coupon/`, config);
+    return response.json;
+};
+
+const createCoupons = async (coupon) => {
+    const response = await axios.post(`${base_url}coupon/`,coupon, config);
+    return response.json;
+}
+
+const couponService = {
+    getCoupons,
+    createCoupons,
+}
+
+
+export default couponService;
