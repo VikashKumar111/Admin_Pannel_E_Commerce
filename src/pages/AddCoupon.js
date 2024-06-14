9e5ac8542081d00450a6021e781fb739d8e75742
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-14 17:01:52 +0530

    add

---
+++ src/pages/AddCoupon.js
@@ -0,0 +1,99 @@
+import React from "react";
+import CustomInput from "../components/Custominput";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { createCoupon, resetState } from "../features/coupon/couponSlice";
+
+let schema = Yup.object().shape({
+  name: Yup.string().required("Coupon Name is Required"),
+  expiry: Yup.date().required("Expiry Date is Required"),
+  discount: Yup.number().required("Discount Percentage is Required"),
+});
+
+const AddCoupon = () => {
+  const dispatch = useDispatch();
+
+  const newBrand = useSelector((state) => state.brand);
+  const { isSuccess, isError, isLoading, createdBrand } = newBrand;
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      expiry: "",
+      discount: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(createCoupon(values));
+      formik.resetForm();
+      notification();
+    },
+  });
+
+  const notification = () => {
+    if (isSuccess && createdCoupon) {
+      toast.success("Coupon Added Successfully!");
+      setTimeout(() => {
+        dispatch(resetState());
+        // navigate("/admin/list-brand");
+      }, 3000);
+    }
+    if (isError) {
+      toast.error("Something Went Wrong!");
+    }
+  };
+
+  return (
+    <div>
+      <h3>Add Coupon</h3>
+      <div>
+        <form action="" onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            name="name"
+            onChng={formik.handleChange("name")}
+            onBlr={formik.handleBlur("name")}
+            val={formik.values.name}
+            label="Enter Coupon Name"
+            id="name"
+          />
+          <div className="error">
+            {formik.touched.name && formik.errors.name}
+          </div>
+          <CustomInput
+            type="date"
+            name="expiry"
+            onChng={formik.handleChange("expiry")}
+            onBlr={formik.handleBlur("expiry")}
+            val={formik.values.expiry}
+            label="Enter Expiry Date"
+            id="date"
+          />
+          <div className="error">
+            {formik.touched.expiry && formik.errors.expiry}
+          </div>
+          <CustomInput
+            type="number"
+            name="discount"
+            onChng={formik.handleChange("discount")}
+            onBlr={formik.handleBlur("discount")}
+            val={formik.values.discount}
+            label="Enter Discount"
+            id="discount"
+          />
+          <div className="error">
+            {formik.touched.discount && formik.errors.discount}
+          </div>
+          <button
+            className="btn btn-success border-0 rounded-3 my-5"
+            type="submit"
+          >
+            Add Coupon
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddCoupon;
