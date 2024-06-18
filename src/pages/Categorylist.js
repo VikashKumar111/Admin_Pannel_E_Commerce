0fc71d34f2e3d0e1f85368ee7c69cdc324147ec3
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-18 18:21:28 +0530

    add

--- src/pages/Categorylist.js
+++ src/pages/Categorylist.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
-import { getCategories } from "../features/pcategory/pcategorySlice";
+import { deleteACategory, getCategories } from "../features/pcategory/pcategorySlice";
 import { BiEdit } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
@@ -20,7 +20,21 @@ const columns = [
 ];
 
 const Categorylist = () => {
+  const [open, setOpen] = useState(false);
+  const [categoryId, setCategoryId] = useState("");
   const dispatch = useDispatch();
+
+
+  const showModal = (id) => {
+    setOpen(true);
+    setCategoryId(id);
+  }
+  console.log(categoryId);
+
+  const hideModal = () => {
+    setOpen(false);
+  }
+
   useEffect(() => {
     dispatch(getCategories());
   }, []);
@@ -35,7 +49,7 @@ const Categorylist = () => {
           <Link className="fs-3 text-danger" to={`/admin/category/${pCatState[i]._id}`}>
             <BiEdit />
           </Link>
-          <button className="ms-3 fs-3 text-danger" to="/">
+          <button onClick={()=> showModal(pCatState[i]._id)} className="ms-3 fs-3 text-danger bg-transparent border-0">
             <AiFillDelete />
           </button>
         </>
@@ -43,18 +57,27 @@ const Categorylist = () => {
     });
   }
 
+  const deleteCategory = (id) => {
+    dispatch(deleteACategory(id));
+
+    setTimeout(() => {
+      setOpen(false);
+      dispatch(getCategories());
+    },400)
+  }
+
   return (
     <div>
       <h3 className="mb-4 title">Product Categories</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
-        <CustomModal
+      </div>
+       <CustomModal
           hideModal={hideModal}
           open={open}
-          performAction={ }
+          performAction={()=>deleteCategory(categoryId)}
           title="Are You Sure You Want To delete This Category?"
         />
-      </div>
     </div>
   );
 };
