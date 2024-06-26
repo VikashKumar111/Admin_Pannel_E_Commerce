a4e04c16305edaab7658e998f9bafce204654701
Vikash Kumar <vikasthakur9310@gmail.com>
2024-06-26 16:36:30 +0530

    add

--- src/features/enquiry/enquiryService.js
+++ src/features/enquiry/enquiryService.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
+import { config } from "../../utils/axiosconfig";
 
 const getEnquiry = async () => {
     const response = await axios.get(`${base_url}enquiry/`);
     return response.data;
 };
 
-
+const deleteEnquiry = async (id) => {
+    const response = await axios.delete(`${base_url}enquiry/${id}`, config);
+    return response.data;
+}
 const enquiryService = {
     getEnquiry,
+    deleteEnquiry,
 };
 
 export default enquiryService;
--- src/features/enquiry/enquirySlice.js
+++ src/features/enquiry/enquirySlice.js
@@ -2,43 +2,75 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import enquiryService from "./enquiryService";
 
 export const getEnquiry = createAsyncThunk(
-    "enquiry/get-enquiry", async (thunkAPI) => {
-        try {
-            return await enquiryService.getEnquiry();
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "enquiry/get-enquiry",
+  async (thunkAPI) => {
+    try {
+      return await enquiryService.getEnquiry();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
-const initialState = {
-    enquiry: [],
-    isLoading: false,
-    isError: false,
-    isSuccess: false,
-    message:"",
-}
+export const deleteAEnquiry = createAsyncThunk(
+  "enquiry/delete-enquiry",
+  async (id, thunkAPI) => {
+    try {
+      return await enquiryService.deleteEnquiry(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
+export const resetState = createAction("Reset_all");
+
+const initialState = {
+  enquiry: [],
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  message: "",
+};
 
 export const enquirySlice = createSlice({
-    name: "enquiry",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(getEnquiry.pending, (state) => {
-            state.isLoading = true;
-        }).addCase(getEnquiry.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isError = false;
-            state.isSuccess = true;
-            state.enquiry = action.payload;
-        }).addCase(getEnquiry.rejected, (state, action) => {
-            state.isLoading = false;
-            state.isError = true;
-            state.isSuccess = false;
-            state.message = action.error;
-        })
-    }
+  name: "enquiry",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getEnquiry.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getEnquiry.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.enquiry = action.payload;
+      })
+      .addCase(getEnquiry.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(deleteAEnquiry.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteAEnquiry.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.deletedEnquiry = action.payload;
+      })
+      .addCase(deleteAEnquiry.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
+  },
 });
 
-export default  enquirySlice.reducer;
+export default enquirySlice.reducer;
