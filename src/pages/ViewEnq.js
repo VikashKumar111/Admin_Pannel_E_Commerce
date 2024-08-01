8f3679d3951d7f837d3eba6f85e9bf8c5466c6b6
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-01 17:48:11 +0530

    add

--- src/pages/ViewEnq.js
+++ src/pages/ViewEnq.js
@@ -20,20 +20,47 @@ const ViewEnq = () => {
       <h3 className="mb-4 title">View Enquiry</h3>
       <div className="mt-5 bg-white p-4 d-flex gap-3 flex-column rounded-3">
         <div className="d-flex align-items-center gap-3">
-          <h5 className="mb-0">Name:</h5>
+          <h6 className="mb-0">Name:</h6>
           <p className="mb-0">{enqName}</p>
         </div>
         <div className="d-flex align-items-center gap-3">
-          <h5 className="mb-0">Mobile:</h5>
+          <h6 className="mb-0">Mobile:</h6>
           <p className="mb-0">
             <a href={`tel:+91${enqMobile}`}>{enqMobile}</a>
           </p>
-              </div>
-               <div className="d-flex align-items-center gap-3">
-          <h5 className="mb-0">Email:</h5>
+        </div>
+        <div className="d-flex align-items-center gap-3">
+          <h6 className="mb-0">Email:</h6>
           <p className="mb-0">
             <a href={`mailto:{enqEmail}`}>{enqEmail}</a>
           </p>
+        </div>
+        <div className="d-flex align-items-center gap-3">
+          <h6 className="mb-0">Comment:</h6>
+          <p className="mb-0">{enqComment}</p>
+        </div>
+        <div className="d-flex align-items-center gap-3">
+          <h6 className="mb-0">Status:</h6>
+          <p className="mb-0">{enqStatus}</p>
+        </div>
+        <div className="d-flex align-items-center gap-3">
+          <h6 className="mb-0">Change Status:</h6>
+          <div>
+            <select
+              name=""
+              defaultValue={enqStatus ? enqStatus : "Submitted"}
+              id=""
+              className="form-control form-select"
+            >
+              <option value="default" selected>
+                Select Status
+              </option>
+              <option value="Submitted">Submitted</option>
+              <option value="Contacted">Contacted</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Resolved">Resolved</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
