eeeea7812ac2297783a1ba7f9d8ae3fad6c0abbd
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-02 12:02:02 +0530

    add

--- src/features/enquiry/enquiryService.js
+++ src/features/enquiry/enquiryService.js
@@ -3,23 +3,36 @@ import { base_url } from "../../utils/base_url";
 import { config } from "../../utils/axiosconfig";
 
 const getEnquiries = async () => {
-    const response = await axios.get(`${base_url}enquiry/`);
-    return response.data;
+  const response = await axios.get(`${base_url}enquiry/`);
+  return response.data;
 };
 
 const deleteEnquiry = async (id) => {
-    const response = await axios.delete(`${base_url}enquiry/${id}`, config);
-    return response.data;
+  const response = await axios.delete(`${base_url}enquiry/${id}`, config);
+  return response.data;
 };
 
 const getEnquiry = async (id) => {
-    const response = await axios.get(`${base_url}enquiry/${id}`);
+  const response = await axios.get(`${base_url}enquiry/${id}`);
+  return response.data;
+};
+
+const updateEnquiry = async (enq) => {
+  const response = await axios.put(
+    `${base_url}enquiry/${enq.id}`,
+    {
+      status: enq.enqData,
+    },
+    config
+    );
     return response.data;
-}
+};
+
 const enquiryService = {
-    getEnquiries,
-    deleteEnquiry,
+  getEnquiries,
+  deleteEnquiry,
     getEnquiry,
+  updateEnquiry,
 };
 
 export default enquiryService;
