8d90238bfed15b861801beee10836bb9988707f0
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-06 17:00:03 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Resetpassword from "./pages/Resetpassword";
-import Forgotpassword from "./pages/Forgotpassword";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
@@ -30,8 +28,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/reset-password" element={<Resetpassword />} />
-        <Route path="/forgot-password" element={<Forgotpassword />} />
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="enquiries" element={<Enquiries />} />
